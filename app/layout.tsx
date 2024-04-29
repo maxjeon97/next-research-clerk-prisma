9bc7e7e0d2634e642ac649a6da0fcf4051aac561
Ben Briones <benbriones50@gmail.com>
2024-04-29 16:22:36 -0700

    added sign-in, sign-up, will be adding sign out next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider>
+        <body className={inter.className}>{children}</body>
+      </ClerkProvider>
     </html>
   );
 }
+
+
+/*
+
+import { SignUp } from "@clerk/nextjs";
+
+
+export default function SignUpPage () {
+    return (
+     <SignUp path="/sign-up" />
+    )
+}
+
+
+import { SignIn } from "@clerk/nextjs"
+
+export default function SignInPage() {
+    return (
+      <SignIn path="/sign-in" />
+    )
+}
+
+*/
